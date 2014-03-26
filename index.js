ad560aee38e68c313f9e542847561713105faf76
Daniel Balcomb <daniel.balcomb@gmail.com>
2014-03-26 19:01:04 +0000

    Added script

---
+++ index.js
@@ -0,0 +1,20 @@
+
+module.exports = compose;
+
+function compose (middleware) {
+  return function* (next) {
+    var i = middleware.length;
+    var prev = next || noop();
+    var curr, type;
+
+    while (i--) {
+      type = typeof middleware[i] === 'object';
+      curr = type ? middleware[i].middleware : middleware[i];
+      prev = curr.call(this, prev);
+    }
+
+    yield* prev;
+  }
+}
+
+function* noop () {}
